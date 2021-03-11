400fd3fa27f95ca7b618c234963b39c5e87999c0
AristaDwijaya <79678943+AristaDwijaya@users.noreply.github.com>
2021-03-11 23:02:41 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!-- my fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+    <!-- my CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Arista Dwijaya</title>
+  </head>
+  <body>
+   
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Arista Dwijaya</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Pricing</a>
+          <a class="nav-item nav-link" href="#">Features</a>
+          <a class="nav-item nav-link" href="#">About</a>
+          <a href="http://localhost:8080/tugas1/formRegistrasi/registrasi.html" class="nav-item btn btn-primary tombol" href="#">Join Us</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- akhir Navbar -->
+
+
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Work <span>Hard</span> During <br> a <span>Pamdemi</span> </h1>
+    </div>
+  </div>
+  <!-- akhir Jumbtron -->
+
+
+  <!-- container -->
+  <div class="countainer">
+
+<!-- Info Panel -->
+  <div class="row justify-content-center">
+    <div class="col-lg-10 info-panel ">
+      <div class="row">
+        <div class="col-lg">
+          <img src="img/jagajarak.png" alt="employee" class="float-left">
+          <h4>Jaga Jarak</h4>
+          <p>jika keluar rumah harus jaga jarak dengan orang lain</p>
+        </div>
+        <div class="col-lg">
+          <img src="img/covid.png" alt="hires" class="float-left">
+          <h4>Jaga kesehatan</h4>
+          <p>dengan menjaga kesehatan kita dapat bekerja dengan baik</p>
+        </div>
+        <div class="col-lg">
+          <img src="img/cucitangan.png" alt="security" class="float-left">
+          <h4>Menjaga Kebersihan</h4>
+          <p>Mencuci tangan habis melakukan pekerjaan di rumah</p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- akhir Info Panel -->
+
+
+<!-- Workingspace -->
+<div class="row workingspace">
+  <div class="col-6">
+    <img src="img/unnamed.jpg" alt="workingspace" class="img-fluid">
+  </div>
+  <div class="col-4">
+    <h3><span>Fibonacci Sequence (Deret angka Fibonacci)</span></h3>
+    <p>Fibonacci Pertama kali dikembangkan oleh Leonardo di Pisa, atau dikenal sebagai Leonardo Fibonacci, seorang ahli matematika pada abad ketiga belas yang menerbitkan buku “Liber Abaci”, deret angka Fibonacci kini digunakan dalam berbagai disiplin ilmu, seperti  biologi, astronomi, geologi, musik, arsitektur, dan finansial. Dalam mengenal fibonacci kita juga harus mengenal Fibonacci Sequence (deret angka Fibonacci)..</p>
+    <a href="http://localhost:8080/tugas3/fibonacci/fibonaci.php#"class="btn btn-primary tombol">Hitung fibonacci</a>
+  </div>
+</div>
+<!-- akhir workingspace -->
+
+  <!-- akhir container -->
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
